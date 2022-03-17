88911c0d26da57b298973a83674132cfb4f7dda1
Kashaf <kashaflareb@gmail.com>
2022-03-17 13:21:06 +0500

    Generate mailer

---
+++ app/mailers/post_question_mailer.rb
@@ -0,0 +1,2 @@
+class PostQuestionMailer < ApplicationMailer
+end
---
+++ test/mailers/post_question_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PostQuestionMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
